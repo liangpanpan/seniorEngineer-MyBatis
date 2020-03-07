deb5f7f3f31f465e53def07d72932fffa558e025
liangpanpan <liangpan8224091@163.com>
2020-03-07 10:24:33 +0800

    test first git project2

--- test-project/src/main/java/com/pp/test/Helloworld.java
+++ test-project/src/main/java/com/pp/test/Helloworld.java
@@ -8,5 +8,6 @@ package com.pp.test;
 public class Helloworld {
     public static void main(String[] args) {
         System.out.println("hello world, this is my project.");
+        System.out.println("2222");
     }
 }
