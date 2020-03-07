43af75bf0f7dba1fa3b740e598b3d871119143fc
liangpanpan <liangpan8224091@163.com>
2020-03-07 10:20:14 +0800

    test first git project

---
+++ test-project/src/main/java/com/pp/test/Helloworld.java
@@ -0,0 +1,12 @@
+package com.pp.test;
+
+/**
+ * @Description TODO
+ * @Author thinkpad
+ * @Date 2020/3/7 10:16
+ */
+public class Helloworld {
+    public static void main(String[] args) {
+        System.out.println("hello world, this is my project.");
+    }
+}
