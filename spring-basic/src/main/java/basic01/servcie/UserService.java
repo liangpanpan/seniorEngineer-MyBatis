b4e86a26bbfc152699a93ae60277e5d82b9735a7
liangpanpan <liangpan8224091@163.com>
2020-09-07 16:00:04 +0800

    添加spring-basic项目

---
+++ spring-basic/src/main/java/basic01/servcie/UserService.java
@@ -0,0 +1,13 @@
+package basic01.servcie;
+
+/**
+ * <pre>
+ * @Version 1.0
+ * Modify Information:
+ * Author       Date          Description
+ * ============ ============= ============================
+ * liangpanpan   2020/9/7       create this file
+ * </pre>
+ */
+public interface UserService {
+}
