e4db58c457d1e3530608f8025effffd2129f4cf6
liangpanpan <liangpan8224091@163.com>
2020-11-09 20:52:59 +0800

    验证Redis集群后

---
+++ spring-basic/src/main/java/com/basic/redis/JedisClusterTest.java
@@ -0,0 +1,58 @@
+package com.basic.redis;
+
+import redis.clients.jedis.HostAndPort;
+import redis.clients.jedis.JedisCluster;
+import redis.clients.jedis.JedisPoolConfig;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * <pre>
+ * @Version 1.0
+ * Modify Information:
+ * Author       Date          Description
+ * ============ ============= ============================
+ * liangpanpan   2020/11/7       create this file
+ * </pre>
+ */
+public class JedisClusterTest {
+
+    public static void main(String[] args) {
+        JedisPoolConfig config = new JedisPoolConfig();
+        config.setMaxTotal(20);
+        config.setMaxIdle(10);
+        config.setMinIdle(5);
+
+        Set<HostAndPort> jedisClusterNode = new HashSet<>();
+        jedisClusterNode.add(new HostAndPort("49.232.17.193", 8001));
+        jedisClusterNode.add(new HostAndPort("49.232.17.193", 8002));
+        jedisClusterNode.add(new HostAndPort("49.232.17.193", 8003));
+        jedisClusterNode.add(new HostAndPort("49.232.17.193", 8004));
+        jedisClusterNode.add(new HostAndPort("49.232.17.193", 8005));
+        jedisClusterNode.add(new HostAndPort("49.232.17.193", 8006));
+
+        JedisCluster jedisCluster = null;
+
+        try {
+            jedisCluster = new JedisCluster(jedisClusterNode, 6000, 5000, 10,
+                    "liangpan", config);
+            System.out.println(jedisCluster.set("cluster", "zhuge"));
+            System.out.println(jedisCluster.get("cluster"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (jedisCluster != null) {
+                try {
+                    jedisCluster.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+
+    }
+
+}
--- springboot-demo-simple01/src/main/java/com/guxingyuan/boot/controller/RedisController.java
+++ springboot-demo-simple01/src/main/java/com/guxingyuan/boot/controller/RedisController.java
@@ -1,11 +1,15 @@
 package com.guxingyuan.boot.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * <pre>
@@ -20,14 +24,31 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping(value = "/redis")
 public class RedisController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(RedisController.class.getName());
+
     @Autowired
     private RedisTemplate redisTemplate;
 
-    @PostMapping(value = "/setRedisInof")
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    @PostMapping(value = "/setRedisInfo")
+    @ResponseBody
     public String setOneRedisInfo(@RequestParam String redisKey, String redisInfo) {
+        LOGGER.info("enter setOneRedisInfo");
         redisTemplate.opsForValue().set(redisKey, redisInfo);
-        return "ok";
+        LOGGER.info("save finish");
+        return (String) redisTemplate.opsForValue().get(redisKey);
     }
 
+
+    @PostMapping(value = "/testStringRedisInfo")
+    @ResponseBody
+    public String testStringRedisInfo(@RequestParam String redisKey, String redisInfo) {
+        LOGGER.info("enter testStringRedisInfo");
+        stringRedisTemplate.opsForValue().set(redisKey, redisInfo);
+        LOGGER.info("save finish");
+        return stringRedisTemplate.opsForValue().get(redisKey);
+    }
 
 }
