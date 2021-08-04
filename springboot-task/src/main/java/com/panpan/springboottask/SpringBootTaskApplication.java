e185343fffad21be3025a81575324832b9a84f62
liangpanpan <liangpan8224091@163.com>
2021-08-04 14:23:10 +0800

    添加springboot定时任务 demo代码

---
+++ springboot-task/src/main/java/com/panpan/springboottask/SpringBootTaskApplication.java
@@ -0,0 +1,21 @@
+package com.panpan.springboottask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <pre>
+ * Modify Information:
+ * Author       Date          Description
+ * ============ ============= ============================
+ * liangpanpan   2021/8/4       create this file
+ * </pre>
+ */
+@SpringBootApplication
+public class SpringBootTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTaskApplication.class, args);
+    }
+
+}
---
+++ springboot-task/src/main/java/com/panpan/springboottask/config/SimpleScheduleConfig.java
@@ -0,0 +1,39 @@
+package com.panpan.springboottask.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.time.LocalDateTime;
+
+/**
+ * 简单定时任务，在代码上进行的表达式cron配置
+ * Cron表达式参数分别表示：
+ * <p>
+ * 秒（0~59） 例如0/5表示每5秒
+ * 分（0~59）
+ * 时（0~23）
+ * 月的某天（0~31） 需计算
+ * 月（0~11）
+ * 周几（ 可填1-7 或 SUN/MON/TUE/WED/THU/FRI/SAT）
+ * second（秒），minute（分），hour（时），day of month（月），month,day of week（周几）
+ * 0 * * * * MON-FRI  周一到周五每一分钟启动一次
+ * * * * * * MON-FRI  周一到周五每一分钟每一秒启动一次
+ *
+ * <pre>
+ * Modify Information:
+ * Author       Date          Description
+ * ============ ============= ============================
+ * liangpanpan   2021/8/4       create this file
+ * </pre>
+ */
+
+@Configuration //1.主要用于标记配置类，兼备Component的效果。
+@EnableScheduling // 2.开启定时任务
+public class SimpleScheduleConfig {
+    //3.添加定时任务
+    @Scheduled(cron = "0/5 * * * * ?")
+    private void configureTasks() {
+        System.err.println("执行定时任务1: " + LocalDateTime.now());
+    }
+}
