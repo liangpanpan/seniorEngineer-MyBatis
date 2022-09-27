5321d422e0415052243650af567155b1656b3f39
liangpanpan <liangpan8224091@163.com>
2022-09-27 16:01:04 +0800

    添加logback加载配置文件说明

--- logback-test/src/main/java/com/guxingyuan/logback/LoggerTest.java
+++ logback-test/src/main/java/com/guxingyuan/logback/LoggerTest.java
@@ -5,6 +5,16 @@ import org.slf4j.LoggerFactory;
 
 /**
  * 默认读取的配置文件是：logback.xml或者logback-test.xml
+ * <p>
+ * logback在启动时：
+ * <p>
+ * 在 classpath 中寻找 logback-test.xml文件
+ * 如果找不到 logback-test.xml，则在 classpath 中寻找 logback.groovy 文件
+ * 如果找不到 logback.groovy，则在 classpath 中寻找 logback.xml文件
+ * 如果上述的文件都找不到，则 logback 会使用 JDK 的 SPI 机制查找 META-INF/services/ch.qos.logback.classic.spi.Configurator 中的 logback
+ * 配置实现类，这个实现类必须实现 Configuration 接口，使用它的实现来进行配置
+ * 如果上述操作都不成功，logback 就会使用它自带的 BasicConfigurator 来配置，并将日志输出到 console
+ *
  * <pre>
  * Modify Information:
  * Author       Date          Description
